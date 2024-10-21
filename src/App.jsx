ea141a606c3c7764508451cce1281a59161640d9
Admin <shivendrawk@gmail.com>
2024-10-21 14:13:56 +0530

    inserting in the mid of an  array in state

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Button from './components/Button';
 // function Item({ name, isPacked}) {
 //   return <li>{isPacked && name + " done"}</lvi>
 // }
-// let nextId =  0;
+let nextId =  3;
 
 //Removing an element from the array
 
@@ -26,12 +26,19 @@ import Button from './components/Button';
 
 //Transforming an array in state
 
-    let initialShapes = [
-      { id: 0, type: 'circle', x: 50, y: 100 },
-      { id: 1, type: 'square', x: 150, y: 100 },
-      { id: 2, type: 'circle', x: 250, y: 100 },
-    ];
+    // let initialShapes = [
+    //   { id: 0, type: 'circle', x: 50, y: 100 },
+    //   { id: 1, type: 'square', x: 150, y: 100 },
+    //   { id: 2, type: 'circle', x: 250, y: 100 },
+    // ];
 
+  //insertig an item in the mid of an array
+
+  const initialArtists = [
+    { id: 0, name: 'Marta Colvin Andrade' },
+    { id: 1, name: 'Lamidi Olonade Fakeye'},
+    { id: 2, name: 'Louise Nevelson'},
+  ];
 
 function App() {
   // const [number, setNumber] = useState(0);
@@ -161,27 +168,47 @@ function App() {
 
       //Transforming the array in state
 
-        const [shapes, setShapes] = useState(
-          initialShapes
-        );
+        // const [shapes, setShapes] = useState(
+        //   initialShapes
+        // );
 
+        // function handleClick() {
+        //   const nextShapes = shapes.map((shape) => {
+        //     if (shape.type === "square") {
+        //       return {
+        //         ...shape,
+        //         x : shape.x + 50,
+        //         y : shape.y + 30,
+        //       }
+        //     } else {
+        //       return {
+        //         ...shape,
+        //         y : shape.y + 50,
+        //       }
+        //     }
+        //   })
+        //   // Re-render with the new array
+        //   setShapes(nextShapes);
+        // }
+
+    // Inserting in the mid of the array
+
+        const [name, setName] = useState('');
+        const [artists, setArtists] = useState(
+          initialArtists
+        );
+      
         function handleClick() {
-          const nextShapes = shapes.map((shape) => {
-            if (shape.type === "square") {
-              return {
-                ...shape,
-                x : shape.x + 50,
-                y : shape.y + 30,
-              }
-            } else {
-              return {
-                ...shape,
-                y : shape.y + 50,
-              }
-            }
-          })
-          // Re-render with the new array
-          setShapes(nextShapes);
+          let insertAt = 1;
+          let nextArtists = [
+            ...artists.slice(0 , insertAt),
+            {id : nextId++, name : name},
+            ...artists.slice(insertAt),
+          ]
+          setArtists(nextArtists);
+          setName('');
+
+          console.log(artists)
         }
 
   return (
@@ -378,7 +405,7 @@ function App() {
 
       {/* Transforming an array */}
 
-          <button onClick={handleClick}>
+          {/* <button onClick={handleClick}>
             Move circles down!
           </button>
           {shapes.map(shape => (
@@ -395,7 +422,23 @@ function App() {
               width: 20,
               height: 20,
             }} />
-          ))}
+          ))} */}
+
+      {/* inserting in the mid of the array in state */}
+
+              <h1>Inspiring sculptors:</h1>
+              <input
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+              <button onClick={handleClick}>
+                Insert
+              </button>
+              <ul>
+                {artists.map(artist => (
+                  <li key={artist.id}>{artist.name}</li>
+                ))}
+              </ul>
             </>
           );
         }
